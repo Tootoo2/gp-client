b9deb90f147dc21a780c1270d5b2382c3d1a7f0a
Anton <antonstromkarl@gmail.com>
2020-04-28 12:57:25 +0200

    added a redirect file to hopefully navigate netlify routing

--- src/ui/theme.js
+++
@@ -1,9 +0,0 @@
-import { createMuiTheme } from "@material-ui/core/styles";
-
-const palette = {
-  primary: { main: "#E1F5FE" },
-  secondary: { main: "#B3E5FC" },
-};
-const themeName = "Pattens Blue French Pass Rock Hyrax";
-
-export default createMuiTheme({ palette, themeName });
